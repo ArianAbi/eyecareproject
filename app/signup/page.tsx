515fb58f5a7a6dad21274f7a1d517be6c583850f
ArianAbi <arian.abolhasanii@gmail.com>
2026-07-20 03:13:10 +0330

    prisma setup

---
+++ app/signup/page.tsx
@@ -0,0 +1,47 @@
+"use client"
+
+import { useState } from "react"
+import { CreateUserr } from "./action"
+
+export default function Signup() {
+
+    const [username, setUsername] = useState("")
+    const [number, setNumber] = useState("")
+    const [password, setPassword] = useState("")
+
+    async function CreateUser() {
+        const result = await CreateUserr({
+            username,
+            number,
+            password
+        })
+
+        console.log(result);
+        alert(result)
+    }
+
+    return (
+        <div>
+            <h1>SignUp</h1>
+
+            <div>
+                <input type="text" placeholder="username"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                />
+                <input type="text" placeholder="phone"
+                    value={number}
+                    onChange={(e) => setNumber(e.target.value)}
+                />
+                <input type="text" placeholder="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+
+                <button onClick={CreateUser}>
+                    Submit
+                </button>
+            </div>
+        </div>
+    )
+}
