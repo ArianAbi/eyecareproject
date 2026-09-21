2bd12f42623ad88ef0327f76e71e39af8a0d506b
ArianAbi <arian.abolhasanii@gmail.com>
2026-09-22 00:42:33 +0330

    admin invoice fix

--- app/admin/invoices/page.tsx
+++ app/admin/invoices/page.tsx
@@ -1,14 +1,17 @@
-import { InvoiceList } from "@/components/core/InvoiceList"
-import type { InvoiceFilters } from "@/lib/invoice-filters"
+import { InvoiceList } from "@/components/core/InvoiceList";
+import type { InvoiceFilters } from "@/lib/invoice-filters";
 
-export default async function AdminInvoicesPage({ searchParams }: { searchParams: Promise<InvoiceFilters> }) {
-    return <div className="space-y-4">
-        <h1 className="text-xl font-semibold">
-            مدیریت صورتحساب‌ها
-        </h1>
-        <InvoiceList
-            admin
-            filters={await searchParams}
-        />
+export default async function AdminInvoicesPage({
+  searchParams,
+}: {
+  searchParams: Promise<InvoiceFilters>;
+}) {
+  return (
+    <div className="space-y-4">
+      <h1 className="text-xl font-semibold">مدیریت صورتحساب‌ها</h1>
+      <InvoiceList admin filters={await searchParams}>
+        <></>
+      </InvoiceList>
     </div>
+  );
 }
